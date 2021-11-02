3f7c2eb60c9a0fccd544e9bf8f9c800fc3069c20
derry <derry_lin@outlook.com>
2021-11-02 11:10:00 +0800

    2021.11.02

--- src/track.py
+++ src/track.py
@@ -817,14 +817,14 @@ if __name__ == '__main__':
                       MOT17-08-SDP
                       MOT17-12-SDP
                       MOT17-14-SDP'''
-        seqs_str = '''MOT17-01-SDP
-                      MOT17-03-SDP
-                      MOT17-06-SDP'''
-        seqs_str = '''MOT17-07-SDP
-                      MOT17-08-SDP
-                      MOT17-12-SDP
-                      MOT17-14-SDP'''
-        seqs_str = '''MOT17-01-SDP'''
+        # seqs_str = '''MOT17-01-SDP
+        #               MOT17-03-SDP
+        #               MOT17-06-SDP'''
+        # seqs_str = '''MOT17-07-SDP
+        #               MOT17-08-SDP
+        #               MOT17-12-SDP
+        #               MOT17-14-SDP'''
+        # seqs_str = '''MOT17-01-SDP'''
         data_root = os.path.join(opt.data_dir, 'MOT17/images/test')
     if opt.val_mot17:
         # seqs_str = '''MOT17-02-SDP
